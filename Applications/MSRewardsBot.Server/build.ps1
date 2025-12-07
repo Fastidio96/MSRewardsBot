6c9ba5452e964fe230994b6d69fac9b06abc3ab7
Fastidio96 <raffaello.perazzi@gmail.com>
2025-12-07 20:37:10 +0100

    Added powershell script for multi platform build

---
+++ Applications/MSRewardsBot.Server/build.ps1
@@ -0,0 +1,21 @@
+# Require Admin / UAC elevation
+If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Host "Restarting script with Administrator privileges..."
+    Start-Process powershell -Verb RunAs -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"" + $MyInvocation.MyCommand.Path + "`"")
+    Exit
+}
+
+$ProjectPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $ProjectPath
+
+# Multi-RID publish
+$RIDs = "win-x64","linux-x64"
+
+foreach ($rid in $RIDs) {
+    Write-Host "`nPublishing for $rid..."
+    dotnet publish -c Release -r $rid -o (Join-Path $ProjectPath "publish\$rid") --self-contained false
+}
+
+Write-Host "`nPublish completed successfully. Press Enter to exit..."
+Read-Host
