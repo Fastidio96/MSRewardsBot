69f63f15821501e1fc750162daf3020513f924d9
Fastidio96 <raffaello.perazzi@gmail.com>
2026-01-20 16:41:21 +0100

    removed using

--- Applications/MSRewardsBot.Client/Windows/SettingsWindow.xaml.cs
+++ Applications/MSRewardsBot.Client/Windows/SettingsWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Net;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using MSRewardsBot.Client.DataEntities;
 
